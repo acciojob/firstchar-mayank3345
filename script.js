b97ed43b35038d7d0ce3bffdd3885c6d1abbc859
Aditya Singh <102604262+mayank3345@users.noreply.github.com>
2026-09-13 16:03:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstChar(text) {
   // your code here
+	let i=0;
+	while(i<text.length-1){
+		if(test[i]!==" "){
+			return text[i];
+			break;
+		}
+		i++;
+	}
+	return "";
 }
 
 // Do not change the code below
